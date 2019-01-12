4941a0cf98330ca59462039a86eb6b7bca43f28d
Willem Scholten <wscholten@seaprep.org>
2019-01-12 13:00:12 -0800

    teachinng code

---
+++ .vscode/cquery_cached_index/@Users@willem@github@seaprep@cortex-teachCWU/src@manipulator.c
@@ -0,0 +1,6 @@
+#include "main.h"    // includes API.h and other headers
+#include "manipulator.h" // ensures that the corresponding header file (manipluator.h) is included
+#include "portdef.h" // All port defintions on the cortex need to be included
+
+// Place all manipluator specific functions here, forexample
+// void flipGrabber(int speed, int rotation) {}
---
+++ .vscode/cquery_cached_index/@Users@willem@github@seaprep@cortex-teachCWU/src@shooter.c
@@ -0,0 +1,6 @@
+#include "main.h"    // includes API.h and other headers
+#include "shooter.h" // ensures that the corresponding header file (shooter.h) is included
+#include "portdef.h" // All port defintions on the cortex need to be included
+
+// Place all shooter specific functions here, forexample
+// void launchBall(int speed) {}
